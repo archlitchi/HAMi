cc3af54e48caf91cd4cbf8e13ed01bdeebf3e846
limengxuan <mengxuan.li@dynamia.ai>
2025-06-27 15:04:41 +0800

    update
    Signed-off-by: limengxuan <mengxuan.li@dynamia.ai>

--- pkg/util/util.go
+++ pkg/util/util.go
@@ -32,7 +32,6 @@ import (
 	corev1 "k8s.io/api/core/v1"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/types"
 	k8stypes "k8s.io/apimachinery/pkg/types"
 	"k8s.io/klog/v2"
 )
@@ -358,7 +357,7 @@ func RemovePodAnnotations(pod *corev1.Pod, key string) error {
 	mkey := strings.ReplaceAll(key, "/", "~1")
 	patchData := fmt.Sprintf("[{\"op\": \"remove\", \"path\":\"/metadata/annotations/%s\"}]", mkey)
 	klog.Infoln("patched data=", string(patchData))
-	_, err := client.GetClient().CoreV1().Pods(pod.Namespace).Patch(context.Background(), pod.Name, types.JSONPatchType, []byte(patchData), metav1.PatchOptions{})
+	_, err := client.GetClient().CoreV1().Pods(pod.Namespace).Patch(context.Background(), pod.Name, k8stypes.JSONPatchType, []byte(patchData), metav1.PatchOptions{})
 	return err
 }
 
